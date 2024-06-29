5b0c254283f74d2b3db479d1fcd4cb93e31ce5b4
Sean <a01080080329@gmail.com>
2024-06-29 20:33:50 +0900

    First_commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float _hAxis;
+    float _vAxis;
+
+    Vector3 _moveVec;
+
+    private void Update()
+    {
+        _hAxis = Input.GetAxisRaw("Horizontal");
+        _vAxis = Input.GetAxisRaw("Vertical");
+
+        _moveVec = new Vector3(_hAxis, 0, _vAxis).normalized;
+
+        transform.position += _moveVec * speed * Time.deltaTime;
+    }
+}
