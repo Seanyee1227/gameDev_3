947f8ec673efa95a2f49b52dde6e26cc20eb2246
Sean <a01080080329@gmail.com>
2024-08-10 01:45:08 +0900

    ChaseSound
    chase상태 소리

--- gameDev_3/Assets/Scripts/Enemy/ChaseState.cs
+++ gameDev_3/Assets/Scripts/Enemy/ChaseState.cs
@@ -1,19 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class ChaseState : StateMachineBehaviour
 {
+    private int _damage = 25;
+    private float _attackDelay = 2f;
+
     NavMeshAgent _agent;
     Transform _target;
+
+    EnemyHealth _enemyHealth;
+    PlayerHealth _playerHealth;
+    [SerializeField]
+    BoxCollider _attackRange;
+
+    [Header("Sound")]
+    [SerializeField]
+    private AudioClip _chaseSound;
+    private AudioSource _audioSource;
+    [SerializeField]
+    [Range(0f, 1f)] // 0 is silent, 1 is full volume
+    private float _volume = 0.5f; // Set this to the desired volume level
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        _enemyHealth = animator.GetComponent<EnemyHealth>();
         _agent = animator.GetComponent<NavMeshAgent>();
         _target = GameObject.FindGameObjectWithTag("Player").transform;
-        _agent.speed = 3.5f;
+        _audioSource = animator.GetComponent<AudioSource>();
+        _agent.speed = 4.5f;
+
+        if (_audioSource != null && _chaseSound != null)
+        {
+            _audioSource.clip = _chaseSound;
+            _audioSource.loop = true;
+            _audioSource.spatialBlend = 1.0f; // 3D sound
+            _audioSource.minDistance = 5f;    // Minimum distance to hear the sound
+            _audioSource.maxDistance = 15f;   // Maximum distance to hear the sound
+            _audioSource.rolloffMode = AudioRolloffMode.Logarithmic; // Rolloff mode
+            _audioSource.volume = _volume; // Set the volume
+            _audioSource.Play();
+        }
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -22,13 +50,21 @@ public class ChaseState : StateMachineBehaviour
         _agent.SetDestination(_target.position);
         float _distance = Vector3.Distance(_target.position, animator.transform.position);
 
-        if (_distance > 15)
+        if (_distance > 15f && !_enemyHealth.isDamaged)
         {
             animator.SetBool("Chasing", false);
+            if (_audioSource != null)
+            {
+                _audioSource.Stop();
+            }
         }
-        if (_distance < 3f)
+        if (_distance < 2.5f)
         {
             animator.SetBool("Attacking", true);
+            if (_audioSource != null)
+            {
+                _audioSource.Stop();
+            }
         }
     }
 
@@ -36,17 +72,10 @@ public class ChaseState : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _agent.SetDestination(animator.transform.position);
+
+        if (_audioSource != null)
+        {
+            _audioSource.Stop();
+        }
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
